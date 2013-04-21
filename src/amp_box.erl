d3de9b50c898491dd9146921d0f1fd08f7888b0c
Dave Peticolas <dave@krondo.com>
2013-04-20 22:43:23 -0700

    work

--- src/amp_box.erl
+++ src/amp_box.erl
@@ -254,7 +254,7 @@ match_kvp(<<>>) ->
 % @doc Consume the given key from the Protocol and return the
 % type of that key and the remaining protocol.
 -spec consume_key(Key::binary(), Protocol::amp_list()) ->
-                         {Type::atom(), NewProtocol::amp_list()}.
+                         {Type::amp_type(), NewProtocol::amp_list()}.
 consume_key(Key, Protocol) ->
     {value, {Key, Type, _Options}, Protocol2} = lists:keytake(Key, 1, Protocol),
     {Type, Protocol2}.
