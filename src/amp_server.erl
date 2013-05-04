191f090456738befcadbdd76ef0eb4c70a853116
Dave Peticolas <dave@krondo.com>
2013-05-04 16:55:29 -0700

    work

---
+++ src/amp_server.erl
@@ -0,0 +1,32 @@
+%% Copyright (c) 2013, Dave Peticolas <dave@krondo.com>
+%%
+%% Permission to use, copy, modify, and/or distribute this software for any
+%% purpose with or without fee is hereby granted, provided that the above
+%% copyright notice and this permission notice appear in all copies.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+%% ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+%% OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+-module(amp_server).
+
+-behaviour(ranch_protocol).
+-behaviour(gen_server).
+
+-record(state, {socket,
+                transport,
+                nextid=0,
+                handler :: atom(),
+                questions :: dict(), % id -> question (pending questions we asked)
+                answers :: dict(), % external id -> answer (pending answers we
+                                   % have been asked),
+                max_pending :: non_neg_integer() % max # of pending q's & a's
+               }).
+
+%% gen_server
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
