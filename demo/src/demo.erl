3f85c37b77c1303c5666b3d68afca034eb5f4119
Dave Peticolas <dave@krondo.com>
2013-05-31 21:54:59 -0700

    work

---
+++ demo/src/demo.erl
@@ -0,0 +1,24 @@
+%% Reuse at will.
+
+%% @private
+-module(demo).
+
+%% API
+-export([sum/4]).
+-export([divide/4]).
+-export([start/0]).
+
+
+-spec sum(inet:ip_address() | inet:hostname(), inet:port_number(),
+          integer(), integer()) -> integer().
+sum(Address, Port, N1, N2) ->
+    ok.
+
+-spec divide(inet:ip_address() | inet:hostname(), inet:port_number(),
+             integer(), integer()) -> integer().
+divide(Address, Port, N1, N2) ->
+    ok.
+
+start() ->
+    ok = application:start(ranch),
+    ok = application:start(demo).
